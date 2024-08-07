2107800680b7c7289cad8f9287230f567a918f44
José Nascimento <joseeantonioo2000@gmail.com>
2024-08-07 16:31:45 -0300

    feat: initialized project with next.js

---
+++ pages/cart.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+
+const cart: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Carrinho</h1>
+        
+    </>
+  );
+}
+export default cart
---
+++ pages/products.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+
+const products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Nossos Produtos</h1>
+        
+    </>
+  );
+}
+export default products
